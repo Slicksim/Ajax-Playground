46c44f9d4f8c26d4da37b771541d85f2b14df509
Simon Cooper <simon@slicksim.co.uk>
2012-04-08 16:57:37 +0100

    Added Solution

---
+++ AjaxPlay/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxPlay.Models;
+
+namespace AjaxPlay.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Form()
+        {
+            return PartialView();
+        }
+
+        public string Update(FormModel model)
+        {
+
+            return "test";
+        }
+
+    }
+}
---
+++ AjaxPlay/Controllers/PanelController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxPlay.Models;
+
+namespace AjaxPlay.Controllers
+{
+    public class PanelController : Controller
+    {
+        //
+        // GET: /Panel/
+
+        public PartialViewResult Panels()
+        {
+            var list = new List<BasePanel>();
+
+            list.Add(new EventsPanel());
+            list.Add(new TasksPanel());
+
+            return PartialView(list);
+        }
+        
+
+    }
+}
